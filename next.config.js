a5fb9cbb82d694af5d0ef4fd71b9aa312797f8d6
kelvinguchu <kelvinguchu5@gmail.com>
2025-01-16 08:33:58 +0300

    Remove next.config.ts and update package.json build script for streamlined installation

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+    };
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
